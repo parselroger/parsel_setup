ffc1814a6fedca4783a62e6d1fe1b5231ec611eb
parselroger <roger@parsel.me>
2015-10-13 19:07:22 -0400

    Update install_packages.rb

--- recipes/install_packages.rb
+++ recipes/install_packages.rb
@@ -17,24 +17,81 @@ end
 
 
 #packages to install
-package ['php5', 
-		 'php5-mysql', 
-		 'php5-memcache', 
-		 'php5-mcrypt', 
-		 'php5-curl',
-		 'php5-gd' ,
-		 'php5-dev', 
-		 'php5-redis',
-		 'apache2',
-		 'libapache2-mod-php5',
-		 'ctags',
-		 'imagemagick',
-		 'mysql-client',
-		 'gcc',
-		 'libpcre3-dev',
-		 'git',
-		 'make',
-		 'php5-imagick',
-		 'php5-geoip']
+package "php5" do
+  action :upgrade
+end
 
+
+package "php5-dev" do
+  action :upgrade
+end
+
+package "php5-mysql" do
+  action :upgrade
+end
+
+package "php5-memcache" do
+  action :upgrade
+end
+
+package "php5-mcrypt" do
+  action :upgrade
+end
+
+package "php5-curl" do
+  action :upgrade
+end
+
+package "php5-gd" do
+  action :upgrade
+end
+
+package "php5-redis" do
+  action :upgrade
+end
+
+package "apache2" do
+  action :upgrade
+end
+
+
+package "libapache2-mod-php5" do
+  action :upgrade
+end
+
+package "ctags" do
+  action :upgrade
+end
+
+package "imagemagick" do
+  action :upgrade
+end
+
+package "mysql-client" do
+  action :upgrade
+end
+
+package "gcc" do
+  action :upgrade
+end
+
+package "libpcre3-dev" do
+  action :upgrade
+end
+
+package "git" do
+  action :upgrade
+end
+
+package "make" do
+  action :upgrade
+end
+
+package "php5-imagick" do
+  action :upgrade
+end
+
+package "php5-geoip" do
+  action :upgrade
+end
 
